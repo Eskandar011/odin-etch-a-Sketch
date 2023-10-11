3cefa809b735e6e923dd76a30456541b1cbcaf6a
Eskandar011 <alejandro.lima11vaz@gmail.com>
2023-10-11 07:53:53 -0600

    Create button 'Selected'

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
- let board = document.querySelector(".board");
+let size = 16;
+
+const selectBtn = document.querySelector(".select");
+selectBtn.addEventListener("click", () => {
+ size = getSize();
+ createBoard(size);
 });
+
+function getSize() {
+ let size = prompt("Give size of board");
+ return size === ""
+  ? alert("You don't enter any value")
+  : size < 0 || size > 100
+  ? alert("Please enter a value between 1 to 100")
+  : size;
+}
+
+function createBoard(size) {
+ let board = document.querySelector(".board");
+ board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+ board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+ let numDivs = size * size;
+
+ for (let i = 0; i < numDivs; i++) {
+  let div = document.createElement("div");
+  board.insertAdjacentElement("beforeend", div);
+ }
+}
